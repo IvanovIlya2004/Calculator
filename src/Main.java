dea348207910d7f027ac1bf2f1004006a4988857
Ilya.Ivanov <ilya.ivanov.ki-25@mail.ru>
2022-12-23 20:57:54 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import input.Lexeme;
+import input.LexemeBuffer;
+import input.StrScanner;
+import java.util.List;
+
+import static input.StrParser.expr;
+import static input.StrParser.search;
+
 public class Main {
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
         StrScanner strScanner = new StrScanner();
-        strScanner.strScanner();
+        String output = strScanner.strScanner();
+        List<Lexeme> lexemes = search(output);
+        LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
+        System.out.println(expr(lexemeBuffer));
     }
 }
---
+++ src/input/StrScanner.java
@@ -0,0 +1,10 @@
+package input; // package for input function
+import java.util.Scanner;
+public class StrScanner {
+    // This method helps you enter data
+    public String strScanner() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+}

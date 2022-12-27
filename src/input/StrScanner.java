7b60780360a5abf341fff9906861312b0c262e53
Ilya.Ivanov <ilya.ivanov.ki-25@mail.ru>
2022-12-28 01:49:14 +0300

    update

--- src/input/StrScanner.java
+++ src/input/StrScanner.java
@@ -1,10 +1,13 @@
 package input; // package for input function
+import output.MainWindowController;
+
 import java.util.Scanner;
 public class StrScanner {
     // This method helps you enter data
     public String strScanner() {
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
+        output.MainWindowController mainWindowController = new MainWindowController();
+
     }
+
 
 }
